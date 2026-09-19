a163a853b715c7a472da44ac16e782f73dba11be
adithyacr45-glitch <adithyacr45@gmail.com>
2026-09-20 00:22:24 +0530

    Bump Android app to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "app.clario.social"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildFeatures { buildConfig = true }
